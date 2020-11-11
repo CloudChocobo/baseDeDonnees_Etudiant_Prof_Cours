901cadab42032f4903c8a6a25e6c461bb63e144a
CloudChocobo <63099285+CloudChocobo@users.noreply.github.com>
2020-11-11 16:19:54 +0100

    Add files via upload

---
+++ basededonnées_ecole_insertions.sql
@@ -0,0 +1,46 @@
+Insert into adresse 
+values 
+('1', 'rue des carottes', '20', 'Nantes'),
+('2', 'rue des choux', '30', 'Nantes'),
+('3', 'rue des petits pois', '10', 'Nantes'),
+('4', 'rue des potimarrons', '40','Nantes');
+
+INSERT INTO etudiant
+ VALUES
+ ('1', 'baba', 'blap', 'Bac +1', '1'),
+ ('2', 'bebe', 'blep', 'bac +1', '2'),
+ ('3', 'bibi', 'blip', 'bac +2', '3'),
+ ('4', 'bobo', 'blop', 'bac +2', '4'),
+ ('5', 'bubu', 'blup', 'bac +3', '3');
+ 
+ INSERT INTO salle
+ VALUES
+ ('1', 'bleu'),
+ ('2', 'rose'),
+ ('3', 'jaune'),
+ ('4', 'noir');
+ 
+ INSERT INTO professeur
+ VALUES
+ ('1', 'Chat', 'Minou', 'Maths'),
+ ('2', 'Chien', 'Rex', 'Histoire'),
+ ('3', 'Oiseau', 'Pioupiou', 'Civilisation'),
+ ('4', 'Elephant', 'Elmer', 'Anglais');
+ 
+ INSERT INTO cours
+ VALUES
+ ('1', '2020-12-12 08:00', '2020-12-12 10:00', '4', '2'),
+ ('2', '2020-12-12 09:00', '2020-12-12 11:00', '2', '4'),
+ ('3', '2020-12-12 11:00', '2020-12-12 13:00','1', '3'),
+ ('4', '2020-12-12 14:00', '2020-12-12 16:00','3', '1');
+ 
+ INSERT INTO participant
+ VALUES
+ ('1', '40', '1', '4'),
+ ('2', '60', '2', '2'),
+ ('3', '15', '3', '1'),
+ ('4', '54', '3', '3'),
+ ('5', '33', '4', '5');
+ 
+ 
+
